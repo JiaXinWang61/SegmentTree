f9e8c72bc6d6a83739f8af9d4d0c0b8dbedf3cfe
JiaXinWang61 <584937529@qq.com>
2020-04-17 16:11:30 +0800

    创建基本方法

---
+++ src/SegmentTree.java
@@ -0,0 +1,33 @@
+
+public class SegmentTree<E> {
+
+    private E[] tree;
+    private E[] data;
+
+    public SegmentTree(E[] arr) {
+        data = (E[]) new Object[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            data[i] = arr[i];
+        }
+        tree = (E[]) new Object[4 * data.length];
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= data.length) {
+            throw new IllegalArgumentException("Index is illegal");
+        }
+        return data[index];
+    }
+
+    public int getSize() {
+        return data.length;
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+}
